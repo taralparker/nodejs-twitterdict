ed58c8597ff1979f5f98c2a33f1ac724bd3068ea
kphongph <kphongph@gmail.com>
2013-12-20 06:42:27 -0600

    12/20

--- twitter_config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key:'4Ac2xs8iHyPVjMSScqeIlQ',
-  consumer_secret:'W1IHE2fFhmIrbt9Z1fUutkVQJVbiup5kMxWGM79BcY',
-  access_token:'80628105-ylsDwRDqeAMw3H3rjwdnBRSFTht2wdvoriTqjjz9s',
-  access_token_secret:'3BoakhEyPMDuJf8rtMe34MNc6bM5Fm652ZXFyt4izGG5T',
-}
