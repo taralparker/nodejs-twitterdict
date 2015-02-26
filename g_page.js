b33cf57a1a368ccf8009ae4eb9b95877c496d76f
Isaac Griswold-Steiner <isaac.griswold.steiner@gmail.com>
2015-02-26 14:18:22 -0600

    Documentation
    Started reading through the code and working on initial documentation.
    Still getting used to js and nodejs, so I'll do a second round when I'm
    more fluent.

--- g_page.js
+++ g_page.js
@@ -1,3 +1,7 @@
+// STATUS : Documentation in progress. Question: Should we refactor stuff like line 11?
+// It doesn't feel right to have a function being called and created at the same time,
+// it doesn't feel modular or clean.
+
 var google = require('./lib/google');
 var natural = require('natural');
 
@@ -5,6 +9,7 @@ var page = 'http://www.nytimes.com/2010/06/28/style/28bully.html?pagewanted=all&
 
 var dict = {};
 
+// I think this is finding all of the terms in a page being pulled by google.text.
 google.text(page, function(text) {
   var tfidf = new natural.TfIdf();
   var count = 0;
